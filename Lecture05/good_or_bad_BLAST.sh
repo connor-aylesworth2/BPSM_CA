411d23656ddb1bd68c29fc305d3c8626ed3a1e85
connor_aylesworth <s2837739@ed.ac.uk>
2025-09-30 12:10:55 +0100

    First attempt at parsing BLAST data

---
+++ Lecture05/good_or_bad_BLAST.sh
@@ -0,0 +1,16 @@
+!#/bin/bash
+unset IFS
+rm *.blastn
+cut -f1,12 blastoutput2.out | grep -v "#" |
+while read bn1 bn2
+do
+	bn2=$(printf %0.f ${bn2})
+	if test ${bn2} -ge 300
+	then
+		echo "HSP bit score ${bn2} is truly excellent"
+		echo -e "${bn1}\t${bn2}" >> good.blastn
+	else
+		echo echo "HSP bit score ${bn2} is not excellent"
+                echo -e "${bn1}\t${bn2}" >> bad.blastn
+	fi
+done
