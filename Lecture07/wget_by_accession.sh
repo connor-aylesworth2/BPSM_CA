0791ba85a6e4b972ade1fafb591346594aaa11da
connor-aylesworth2 <cjohnayle6@gmail.com>
2025-10-26 10:38:06 +0000

    updating L07 script

---
+++ Lecture07/wget_by_accession.sh
@@ -0,0 +1,6 @@
+while read accession
+ do
+ echo -e "Downloading ${accession} ..."
+ wget -O ${accession}.fasta \
+ "https://eutils.ncbi.nlm.nih.gov/entrez/eutils/efetch.fcgi?db=protein&id=${accession}&strand=1&rettype=fasta&retmode=text"
+
